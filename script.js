7c11ffd633c0214bd23741ab570c27f0fd6209ab
karamhaddad <karam.haddad.30@gmail.com>
2024-02-11 12:17:37 -0600

    Not sure why all files disappeared. added files back.

---
+++ script.js
@@ -0,0 +1,31 @@
+const exp_pre_btn = document.getElementById('exp-pre-btn');
+const exp_nxt_btn = document.getElementById('exp-nxt-btn');
+const experience_container = document.getElementById('experience-container');//experience section
+
+const cardWidth = 270; //wifth of card from CSS... .experience-card accounts for width and paddign
+
+exp_pre_btn.addEventListener('click', () => {
+  experience_container.scrollLeft -= cardWidth;
+});
+
+exp_nxt_btn.addEventListener('click', () => {
+  experience_container.scrollLeft += cardWidth;
+});
+
+//Now code for projects section
+const proj_pre_btn = document.getElementById('proj-pre-btn');
+const proj_nxt_btn = document.getElementById('proj-nxt-btn');
+const projects_container = document.getElementById('projects-container');//projects section
+
+//same cardwidth as experience. so use same variable
+
+proj_pre_btn.addEventListener('click', () => {
+  projects_container.scrollLeft -= cardWidth;
+});
+
+proj_nxt_btn.addEventListener('click', () => {
+  projects_container.scrollLeft += cardWidth;
+});
+
+
+console.log('Script loaded!'); //for testing to make sure script is runnign
