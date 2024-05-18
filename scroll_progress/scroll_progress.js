7952c1835f362c3ddd7aa187ce4b97d762602fbf
karamhaddad <karam.haddad.30@gmail.com>
2024-05-18 02:09:36 -0500

    footer and small fixes

--- scroll_progress/scroll_progress.js
+++ scroll_progress/scroll_progress.js
@@ -4,6 +4,5 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  console.log(scrolled); // For debugging
   document.getElementById("myBar").style.width = scrolled + "%";
 }
